e0f1bf001f95adc7d328cfa5631caa17d96c1612
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-05-23 13:37:23 +0300

    Add view form

--- mpesa_api/urls.py
+++ mpesa_api/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url('c2b/validate/', C2bValidation.as_view(), name='c2b_validation'),
     url('c2b/online_checkout/callback/', OnlineCheckoutCallback.as_view(), name='c2b_checkout_callback'),
     url('payment/', Order.as_view(), name='mpesa_payment'),
+    url('payment_request/', MpesaPayment.as_view(), name='mpesa_request'),
+
 ]
--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -7,6 +7,7 @@ from mpesa_api.tasks import process_b2c_result_response_task, \
     handle_online_checkout_callback_task
 from mpesa_api.mpesa import Mpesa
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from edxmako.shortcuts import render_to_string
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
 
@@ -17,6 +18,7 @@ class B2cTimeOut(APIView):
     """
     Handle b2c time out
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     @csrf_exempt
     def post(self, request, format=None):
         """
@@ -33,6 +35,7 @@ class B2cResult(APIView):
     """
     Handle b2c result
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     @csrf_exempt
     def post(self, request, format=None):
         """
@@ -52,6 +55,7 @@ class C2bValidation(APIView):
     """
     Handle c2b Validation
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     @csrf_exempt
     def post(self, request, format=None):
         """
@@ -71,6 +75,7 @@ class C2bConfirmation(APIView):
     """
     Handle c2b Confirmation
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     @csrf_exempt
     def post(self, request, format=None):
         """
@@ -90,6 +95,7 @@ class OnlineCheckoutCallback(APIView):
     """
     Handle online checkout callback
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     @csrf_exempt
     def post(self, request, format=None):
         """
@@ -120,6 +126,43 @@ class Order(APIView):
         :return:
         """
         data = request.data
+        return render_to_string('mpesa/mpesa_form.html', {
+            'action': '/mpesa/payment_request/',
+            'params': {
+                'cmd': '_xclick',
+                'charset': 'utf-8',
+                'currency_code': data['currency_code'],
+                'amount': data['amount'],
+                'item_name': data['item_name'],
+                'custom': data['custom'],
+                'business': data['business'],
+                'notify_url': data['notify_url'],
+                'cancel_return': data['cancel_return'],
+                'return': data['return'],
+            },
+            'fields': {
+                'phone' : data['phone'],
+            }
+        })
+
+
+
+
+class MpesaPayment(APIView):
+    """
+
+    """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    @csrf_exempt
+    def post(self, request, format=None):
+        """
+        process the confirmation
+        :param request:
+        :param format:
+        :return:
+        """
+        data = request.data
 
         data['payment_status'] = 'Completed'
         amount, account_reference = data['amount'], data['item_name']
@@ -129,6 +172,6 @@ class Order(APIView):
             Mpesa.c2b_register_url()  # registers the validate and confirmation url's for b2c
             # starts online checkout on given number
             Mpesa.stk_push(254700000000, amount, account_reference=account_reference)
-            return  Response(dict(value='ok', key='status', detail='success')) #TODO: change to return true dict
+            return Response(dict(value='ok', key='status', detail='success'))  # TODO: change to return true dict
         else:
-            return  Response(dict(value='fail', key='status', detail='fail')) #TODO: change to return false dict
+            return Response(dict(value='fail', key='status', detail='fail'))  # TODO: change to return false dict
