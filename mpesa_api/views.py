edf318a7517505e7957da2ff3e23ae3c553a07ac
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-06-13 13:53:16 +0300

    delete not digit from phone

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -171,12 +171,12 @@ class MpesaPayment(APIView):
             response.delete()
         except:
             pass
-
+        phone = ''.join(x for x in data['phone'] if x.isdigit())
         if amount and account_reference:
-            Mpesa.b2c_request(data['phone'], amount)  # starts a b2c payment
+            Mpesa.b2c_request(phone, amount)  # starts a b2c payment
             Mpesa.c2b_register_url()  # registers the validate and confirmation url's for b2c
             # starts online checkout on given number
-            Mpesa.stk_push(data['phone'], amount, account_reference=account_reference, orderId = data['custom'])
+            Mpesa.stk_push(phone, amount, account_reference=account_reference, orderId = data['custom'])
             return Response(dict(value='ok', key='status', detail='success', check_url=reverse('check_payment_order')))  # TODO: change to return true dict
         else:
             return Response(dict(value='fail', key='status', detail='fail', check_url=""))  # TODO: change to return false dict
