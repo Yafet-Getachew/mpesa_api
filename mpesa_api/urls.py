58a515a9b9fc7db31e96ec1b5aaede0cd08cca73
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-08-27 12:54:31 +0300

    fix url pattern

--- mpesa_api/urls.py
+++ mpesa_api/urls.py
@@ -5,13 +5,13 @@ from mpesa_api.views import B2cResult, B2cTimeOut, C2bConfirmation, C2bValidatio
 app_name = 'mpesa'   
 
 urlpatterns = [
-    url('b2c/timeout', B2cTimeOut.as_view(), name='b2c_timeout'),
-    url('b2c/result', B2cResult.as_view(), name='b2c_result'),
-    url('c2b/confirmation', C2bConfirmation.as_view(), name='c2b_confirmation'),
-    url('c2b/validate', C2bValidation.as_view(), name='c2b_validation'),
-    url('c2b/online_checkout/(?P<order_id>[0-9]+)/callback', OnlineCheckoutCallback.as_view(), name='c2b_checkout_callback'),
-    url('payment/', Order.as_view(), name='mpesa_payment'),
-    url('payment_request/', MpesaPayment.as_view(), name='mpesa_request'),
-    url('check_payment_order/', CheckStatusOfPayment.as_view(), name="check_payment_order")
+    url('^b2c/timeout', B2cTimeOut.as_view(), name='b2c_timeout'),
+    url('^b2c/result', B2cResult.as_view(), name='b2c_result'),
+    url('^c2b/confirmation', C2bConfirmation.as_view(), name='c2b_confirmation'),
+    url('^c2b/validate', C2bValidation.as_view(), name='c2b_validation'),
+    url('^c2b/online_checkout/(?P<order_id>[0-9]+)/callback', OnlineCheckoutCallback.as_view(), name='c2b_checkout_callback'),
+    url('^payment/', Order.as_view(), name='mpesa_payment'),
+    url('^payment_request/', MpesaPayment.as_view(), name='mpesa_request'),
+    url('^check_payment_order/', CheckStatusOfPayment.as_view(), name="check_payment_order")
 
 ]
